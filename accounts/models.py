e04fd9d142083fa814ea9314c334d1d5be11cb90
CursedGreanbeen <m.adelina_2003@mail.com>
2026-02-24 02:19:51 +0500

    User model added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+ROLE_CHOICES = [
+    ('admin', 'Administrator'),
+    ('manager', 'Manager'),
+    ('user', 'User'),
+    ('guest', 'Guest'),
+    ]
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)  # check
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    patronymic = models.CharField(max_length=100)
+    role = models.CharField(choices=ROLE_CHOICES, default='guest')
+    is_active = models.BooleanField(default=True)
