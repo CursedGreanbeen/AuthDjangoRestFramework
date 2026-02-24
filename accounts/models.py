07ac928eab5b8481d33acf6430803aec376fb16a
CursedGreanbeen <m.adelina_2003@mail.com>
2026-02-24 16:14:04 +0500

    Extended class User, added registration function

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+import bcrypt
 from django.db import models
 
 
@@ -8,11 +9,31 @@ ROLE_CHOICES = [
     ('guest', 'Guest'),
     ]
 
+
 class User(models.Model):
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=50)  # check
+    password = models.CharField(max_length=50)  # check length
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     patronymic = models.CharField(max_length=100)
     role = models.CharField(choices=ROLE_CHOICES, default='guest')
     is_active = models.BooleanField(default=True)
+
+    def cypher_password(self, password):
+        salt = bcrypt.gensalt()
+        self.hash_password = bcrypt.hashpw(
+            password.encode('utf-8'),
+            salt
+        ).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.checkpw(
+            password.encode('utf-8'),
+            self.hash_password.encode('utf-8')
+        )
+
+    def return_json(self):
+        pass
+
+    def save_user(self):
+        pass
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json
+import requests
 
-# Create your views here.
+from django.http import JsonResponse
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import User
+
+
+def register_new_user(request):
+    if request.method == 'POST':
+        try:
+            response = requests.get(url, timeout=5)
+            data = json.loads(request.body)
+            new_user = User()
+            new_user.email = data.get('email')
+            if User.objects.filter(email=new_user.email).exists():
+                new_user.return_json('email', 'This email address already exists!')
+            else:
+                new_user.name = data.get('name')
+                new_user.surname = data.get('surname')
+                new_user.patronymic = data.get('patronymic')
+                new_user.password = new_user.cypher_password(data.get('password'))
+                new_user.save()
+            return response.status_code
+
+        except:
+            pass
